fa645c4c4466970d43cf39ec2026e4618d731ba3
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-26 13:07:56 +0700

    create project => start backend

---
+++ src/main/java/com/project/ecommerce_website_backend/EcommerceWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebsiteBackendApplication.class, args);
+	}
+
+}
